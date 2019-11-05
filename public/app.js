8f12bf7ee1583c27cee39237cffe12b995cc25d5
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-11-05 13:42:20 +0400

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -11,5 +11,9 @@ function somthing()
 
 function add_to_cart(id)
 {
-	alert('Вы добавили пиццу id: ' + id);
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
