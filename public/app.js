6d668f6aeb600a63e0614c6c583385e2889455d8
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-11-06 10:58:21 +0400

    update orders button

--- public/app.js
+++ public/app.js
@@ -20,13 +20,21 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
-	update_orders_input()
+	update_orders_input();
+	update_orders_button();
 }
 
 function update_orders_input()
 {
 	var orders = cart_get_number_orders();
 	$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() +')';
+	$('#orders_button').val(text);
+
 }
 
 function cart_get_number_of_items()
